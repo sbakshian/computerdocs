c850b21ac24e0e6dd449469987d751c9feb7fd92
Sam Bakshian <sam@nordeastdesign.com>
2013-03-04 10:57:40 -0600

    First commit for Computer DOCS

---
+++ sites/all/themes/computerdocs2_theme/template.php
@@ -0,0 +1,7 @@
+<?php
+function computerdocs2_theme_preprocess_page(&$variables) {
+  if (isset($variables['node'])) {
+    $variables['theme_hook_suggestions'][] = 'page__'. str_replace('_', '--', $variables['node']->type);
+  }
+}
+?>
